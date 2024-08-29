0a35ba470a68106f24fae70e118d95990f7a86b7
Manoj Upadhya <70696140+manojupadhya11@users.noreply.github.com>
2024-08-29 09:54:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def create_app():
 
     @app.route('/')
     def home():
-        return 'Hello to Manoj DevOps World, welcome to Manipal Institute of Technology, I am a Cloud-Devops Student'
+        return 'Hello from Manoj to DevOps World, welcome to Manipal Institute of Technology, I am a Cloud-Devops Student, Exploring the beautiful Cloud-DevOps World'
         
     return app 
 
