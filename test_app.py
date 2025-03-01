a89c089eac17d7e96728fb645470df5638eeecfa
Manoj Upadhya <70696140+manojupadhya11@users.noreply.github.com>
2025-03-01 21:22:33 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -16,7 +16,7 @@ def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
 
-    expected_text = 'HI MANOJ UPADHYA SAYS HE IS GOOD DEVOPS ENGINNER WHO LOVES CONTINOUS LEARNING1234512e3431234'
+    expected_text = 'HI MANOJ UPADHYA SAYS HE IS GOOD DEVOPS ENGINNER WHO LOVES CONTINOUS LEARNING'
 #Comment
 
     print(response.data)
