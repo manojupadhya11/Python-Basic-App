92e27233682b84aaef83b18e94288211cef2aae7
Manoj Upadhya <70696140+manojupadhya11@users.noreply.github.com>
2025-03-01 08:25:48 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -16,7 +16,7 @@ def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
 
-    expected_text = 'Hi World Manoj welcomes you to world of devops'
+    expected_text = 'HI MANOJ UPADHYA SAYS HE IS GOOD DEVOPS ENGINNER WHO LOVES CONTINOUS LEARNING'
 #Comment
 
     print(response.data)
